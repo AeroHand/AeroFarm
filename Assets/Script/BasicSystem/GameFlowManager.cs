1c8dc165b753944e5afa4a9dc362d61740d76d70
Peter Tang <peter@narvalous.com>
2016-11-15 18:15:14 -0800

    some init prefabs

---
+++ Assets/Script/BasicSystem/GameFlowManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameFlowManager : MonoBehaviour {
+
+    //public prefab here
+    public GameObject grid_lefttop;
+    public GameObject grid_left;
+    public GameObject grid_leftbot;
+    public GameObject grid_bot;
+    public GameObject grid_rightbot;
+    public GameObject grid_right;
+    public GameObject grid_righttop;
+    public GameObject grid_top;
+    public GameObject grid_dirt;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Script/BasicSystem/GridSystem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridSystem : MonoBehaviour {
+
+   
+
+	// Use this for initialization
+	void Start () {
+	    //generate init room
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
